0b90e67eb6a9c205cda2023de0faf1ae3133ddc4
Yulia Protsenko <prooyuul@gmail.com>
2024-05-13 15:24:39 +0300

    Initial commit

---
+++ src/test/java/com/practice/webchat/WebChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.webchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
